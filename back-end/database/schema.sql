86bb0d3543725d490f6d1ba1c2886956a27e64fb
Abdurhman Alsamraie <aalsamma@buffalo.edu>
2019-02-25 12:43:03 -0500

    Fix database schema and change directory location for easy access

--- back-end/database/schema.sql
+++
@@ -1,7 +0,0 @@
-DROP TABLE IF EXISTS Users;
-Create table IF NOT EXISTS Users (
-    user_id integer primary key autoincrement,
-    name text not null,
-    email text not null,
-    hashed_password text not null
-);
