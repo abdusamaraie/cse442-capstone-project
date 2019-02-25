31a94bc58bf93175013f8f91667cf09ddb6f13b6
Abdurhman Alsamraie <aalsamma@buffalo.edu>
2019-02-25 12:47:22 -0500

    Impelment add_user method to add new user to the Sqlite database

--- back-end/server/helpers/sqlite.py
+++ back-end/server/helpers/sqlite.py
@@ -1,9 +1,23 @@
 from objects.user import User
 
-def add_user(user):
-    print('Add user to sqlite')
+import sqlite3 as sql
 
-    # return None if error
+def add_user(user):
+
+    con = sql.connect("database/users.db")
+    cur = con.cursor()
+
+    try:
+        cur.execute("INSERT INTO Users (email,hashed_password) VALUES (?,?)", (user.username, user.password_hash))
+        con.commit()
+        return True
+    except Exception as e:
+        return None  # return None if error
+    finally:
+        cur.close()
+        con.close()
+
+
 
 def post_message(user, location, message):
 
