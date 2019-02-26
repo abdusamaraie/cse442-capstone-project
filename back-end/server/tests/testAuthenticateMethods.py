13f4d4e184897687254da9d5c00c29a415dcc177
Abdurhman Alsamraie <aalsamma@buffalo.edu>
2019-02-26 09:06:26 -0500

    Add unittest for hash_password method

--- back-end/server/tests/testAuthenticateMethods.py
+++ back-end/server/tests/testAuthenticateMethods.py
@@ -5,11 +5,14 @@ from helpers import authenticate
 class TestAuthenticateMethods(unittest.TestCase):
     def test_verify_user(self):
         #get username and password from database
-        tempUserName = "abd@gmail.com"
+        tempUserName = "abd"
         tempPassword = "123"
         #code here
         self.assertTrue(authenticate.verify_user(tempUserName,tempPassword))
 
+    def test_generate_hash(self):
+        tempUserPassword = "123"
+        self.assertTrue(authenticate.generate_hash(tempUserPassword))
 
 if __name__ == '__main__':
         unittest.main()
