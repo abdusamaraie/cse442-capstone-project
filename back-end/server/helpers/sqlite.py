7f944fe7b0b987dd0673221af6c635221a9c2d44
Darren Matthew <djmatthe@buffalo.edu>
2019-02-26 14:30:39 -0500

    Implemented get_messages. This is curretly returning all messages regardless of distance, next will be finding messages in a radius

--- back-end/server/helpers/sqlite.py
+++ back-end/server/helpers/sqlite.py
@@ -19,3 +19,22 @@ def post_message(user, location, message):
     print('Add message to message table')
 
     # return None if error
+
+
+def get_messages(location, distance):
+    con = get_db()
+    cur = con.cursor()
+
+    try:
+        query = cur.execute("SELECT * FROM Post")  # currently grabbing all messages
+        # return messages in json
+        rv = query.fetchall()
+        ret_json = json.dumps(rv)
+        return ret_json
+    except Exception as e:
+        return None  # return None on error
+    finally:
+        cur.close()
+        con.close()
+
+    # print('Get messages within users defined distance')
