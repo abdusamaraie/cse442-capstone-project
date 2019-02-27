df4a017b27c0dc3be722e438493994800b90ebc3
Baily Troyer <bailytroyer@Bailys-MacBook-Pro.local>
2019-02-26 20:42:23 -0500

    Adding Feed Cell to be used within feed table view controller

---
+++ Front-end/MapChat/MapChat/Storyboards/Main/Views/FeedViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  FeedViewCell.swift
+//  MapChat
+//
+//  Created by Baily Troyer on 2/26/19.
+//  Copyright © 2019 CSE442Group. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class FeedViewCell: UITableViewCell {
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+    
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+        
+        // Configure the view for the selected state
+    }
+}
