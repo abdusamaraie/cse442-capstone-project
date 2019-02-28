4defff0388df70006cbf87cb5bb51f50ee56164b
hard2code <aalsamma@buffalo.edu>
2019-02-27 21:07:32 -0500

    add setUser method to fill user information for signup

--- back-end/server/objects/user.py
+++ back-end/server/objects/user.py
@@ -1,8 +1,18 @@
+from helpers import authenticate
 class User:
     def __init__(self, username, password_hash=None):
 
         if not password_hash:
-            password_hash = ''
+            self.password_hash = ''
 
         self.username = username
         self.password_hash = password_hash
+        self.fullname = ''
+        self.email = ''
+
+    def setUser(self,fullname, email, username,password=None):
+        self.fullname = fullname
+        self.email = email
+        self.username = username
+        self.password_hash = authenticate.generate_hash(password)
+
