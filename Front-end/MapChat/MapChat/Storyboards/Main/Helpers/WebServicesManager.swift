02356abfac45a6e9f435af68e37188972640b5d2
Baily Troyer <bailytroyer@Bailys-MacBook-Pro.local>
2019-02-27 19:47:12 -0500

    Adding profile view with the corresponding templated UI helper methods to retrieve data from backend.
    -AppState will record all metadata queries that retrieve user information, message information
    -LocationServicesManager will track the location and eventually deal with messageKit and CoreData
    -WebServicesManager will send and retrieve all requests from the Flask instance

--- Front-end/MapChat/MapChat/Storyboards/Main/Helpers/WebServicesManager.swift
+++ Front-end/MapChat/MapChat/Storyboards/Main/Helpers/WebServicesManager.swift
@@ -21,19 +21,67 @@ class WebServicesManager{
     static func testAlamofire() {
         
         Alamofire.request(serverIpAddress).responseJSON { response in
-            print("Request: \(String(describing: response.request))")   // original url request
-            print("Response: \(String(describing: response.response))") // http url response
-            print("Result: \(response.result)")                         // response serialization result
+            
+            // original url request
+            print("Request: \(String(describing: response.request))")
+            
+            // http url response
+            print("Response: \(String(describing: response.response))")
+            
+            // response serialization result
+            print("Result: \(response.result)")
             
             if let json = response.result.value {
-                print("JSON: \(json)") // serialized json response
+                
+                // serialized json response
+                print("JSON: \(json)")
             }
             
             if let data = response.data, let utf8Text = String(data: data, encoding: .utf8) {
+                
+                // original server data as UTF8 string
                 print("Data: \(utf8Text)") // original server data as UTF8 string
+            }
+        }
+        
+    }
+    
+    static func getFeed(latLong: (Double, Double), completionHandler: @escaping (NSDictionary?, Error?) -> ()) {
+        let parameters: Parameters = ["latlong": latLong]
+        
+        Alamofire.request(serverIpAddress, method: .get, parameters: parameters).validate().responseJSON { response in
+            
+            guard response.result.isSuccess else {
+                print("Error while fetching remote rooms: \(String(describing: response.result.error))")
+                return
+            }
+            
+            guard let value = response.result.value as? [String: Any],
+                let _ = value["rows"] as? [[String: Any]] else {
+                    print("Malformed data received from fetchAllRooms service")
+                    return
             }
         }
     }
     
+    static func getUser(username:String, completionHandler: @escaping (NSDictionary?, Error?) -> ()) {
+        
+        let parameters: Parameters = ["username": username]
+        
+        Alamofire.request(serverIpAddress, method: .get, parameters: parameters).validate().responseJSON { response in
+            
+            guard response.result.isSuccess else {
+                print("Error while fetching remote rooms: \(String(describing: response.result.error))")
+                return
+            }
+            
+            guard let value = response.result.value as? [String: Any],
+                let _ = value["rows"] as? [[String: Any]] else {
+                    print("Malformed data received from fetchAllRooms service")
+                    return
+                }
+        }
+        
+    }
     
 }
