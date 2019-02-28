1834dd44f5bc9476d34ecd00596c3004100d6b75
hard2code <aalsamma@buffalo.edu>
2019-02-28 11:25:50 -0500

    update database schema to add post and users tables

--- back-end/server/database/schema.sql
+++ back-end/server/database/schema.sql
@@ -1,7 +1,18 @@
 DROP TABLE IF EXISTS Users;
 Create table IF NOT EXISTS Users (
     user_id integer primary key autoincrement,
-    fullname text,
+    fname text,
+    lname text,
     username text not null unique,
     hashed_password text not null
+);
+
+DROP TABLE IF EXISTS post;
+CREATE TABLE post (
+    postId    INTEGER      PRIMARY KEY AUTOINCREMENT,
+    latitude  DOUBLE,
+    longitude DOUBLE,
+    content   TEXT,
+    time      DATETIME,
+    uname     VARCHAR (30) REFERENCES user (username) ON DELETE CASCADE
 );
