6dd55e2ab06300818762d27fe85a40c5fad2ba8e
hard2code <aalsamma@buffalo.edu>
2019-02-28 08:30:49 -0500

    Modified helpers methods and add get_user method to retrieve user object

--- back-end/server/helpers/authenticate.py
+++ back-end/server/helpers/authenticate.py
@@ -2,9 +2,10 @@ import hashlib
 import base64
 import uuid
 import sqlite3 as sql
+from constants.constants import HASH_PASSWORD_PATH,DATABASE_PATH
 
 def generate_hash(password):
-    tf_out = open('database/user_hash_passwords.txt','wb')
+    tf_out = open(HASH_PASSWORD_PATH,'wb')
 
     salt = base64.urlsafe_b64encode(uuid.uuid4().bytes)
 
@@ -20,11 +21,11 @@ def generate_hash(password):
 
 def verify_user(username, password_hash):
 
-    con = sql.connect("database/users.db")
+    con = sql.connect(DATABASE_PATH)
     cur = con.cursor()
 
     try:
-        login = cur.execute('SELECT * from Users WHERE email = ? AND hashed_password = ?', (username, password_hash))
+        login = cur.execute('SELECT * from Users WHERE username = ? AND hashed_password = ?', (username, password_hash))
         if (len(login.fetchall()) > 0):
             return True
         else:
--- back-end/server/helpers/sqlite.py
+++ back-end/server/helpers/sqlite.py
@@ -1,22 +1,52 @@
 from objects.user import User
-
+from constants.constants import DATABASE_PATH
 import sqlite3 as sql
 
+
+# connect to database
+def get_db():
+    con = sql.connect(DATABASE_PATH)
+    return con
+
 def add_user(user):
-
-    con = sql.connect("database/users.db")
+    #setup database connection
+    con = get_db()
     cur = con.cursor()
-
+    #do database query
     try:
-        cur.execute("INSERT INTO Users (email,hashed_password) VALUES (?,?)", (user.username, user.password_hash))
+        cur.execute("INSERT INTO Users (username,hashed_password) VALUES (?,?)", (user.username, user.password_hash))
         con.commit()
         return True
+
     except Exception as e:
         return None  # return None if error
     finally:
         cur.close()
-        con.close()
+        con.close() #close connection
 
+#get user data from database by username
+def get_user(username):
+    # setup database connection
+    con = get_db()
+    cur = con.cursor()
+    user = User(username)
+
+    # do database query
+    try:
+        curs = cur.execute("SELECT * from Users WHERE username = ? ", (username,))
+        user_info = curs.fetchall()
+        if (len(user_info) > 0):
+            user.setUser(user_info[0][1],user_info[0][2],user_info[0][3])
+            #return user object
+            return user
+        else:
+            return False
+
+    except Exception as e:
+        return False  # return None if error
+    finally:
+        cur.close()
+        con.close() #close connection
 
 
 def post_message(user, location, message):
