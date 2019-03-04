38542f2137552821d843cdaea61c1a63759801cf
hard2code <aalsamma@buffalo.edu>
2019-03-03 21:47:46 -0500

    Create upload directory, -update constats with upload path

--- back-end/server/constants/constants.py
+++ back-end/server/constants/constants.py
@@ -1,5 +1,5 @@
 test = 'test'
-DATABASE_PATH = 'database/main.db'
+DATABASE_PATH = 'database/users.db'
 UPLOAD_PATH = 'database/uploads/'
 HASH_PASSWORD_PATH = 'database/user_hash_passwords.txt'
 DEGREE_CONVERSION = 111111
