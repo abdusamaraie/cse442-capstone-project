b91116fa5f769d724a97368829752749b1cbe54a
hard2code <aalsamma@buffalo.edu>
2019-03-04 18:35:28 -0500

    add databas file and schema back to working directory

--- back-end/server/database/schema.sql
+++ back-end/server/database/schema.sql
@@ -1,18 +1,19 @@
 DROP TABLE IF EXISTS Users;
-CREATE TABLE IF NOT EXISTS Users (
+CREATE TABLE Users (
     user_id INTEGER PRIMARY KEY AUTOINCREMENT,
     fname TEXT,
     lname TEXT,
+    photo_url TEXT,
     username TEXT NOT NULL UNIQUE,
     hashed_password TEXT NOT NULL
 );
 
-DROP TABLE IF EXISTS post;
-CREATE TABLE post (
+CREATE TABLE Posts (
     postId    INTEGER      PRIMARY KEY AUTOINCREMENT,
     latitude  DOUBLE,
     longitude DOUBLE,
     content   TEXT,
     time      DATETIME,
-    uname     VARCHAR (30) REFERENCES user (username) ON DELETE CASCADE
+    uname     VARCHAR (30) REFERENCES Users (username) ON DELETE CASCADE
+
 );
