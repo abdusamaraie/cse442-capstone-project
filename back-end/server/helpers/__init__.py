5f56cfb1f98b496cd0e789c318f3354b9d3a4d49
Darren Matthew <djmatthe@buffalo.edu>
2019-03-16 22:04:14 -0400

    moved init.py

---
+++ back-end/server/helpers/__init__.py
@@ -0,0 +1,9 @@
+from helpers.neo4j import GRAPH
+
+# setup uniqueness constraints on database
+GRAPH.schema.create_uniqueness_constraint("User", "username")
+GRAPH.schema.create_uniqueness_constraint("User", "email")
+GRAPH.schema.create_uniqueness_constraint("Reply", "reply_id")
+GRAPH.schema.create_uniqueness_constraint("Post", "post_id")
+
+GRAPH.run("CALL spatial.addPointLayer('posts')")
--- back-end/server/server.py
+++ back-end/server/server.py
@@ -169,8 +169,8 @@ def delete_reply():
 
 
 def start_server():
-    app.run(host='0.0.0.0', port=80, debug=True)
-    # app.run(host='127.0.0.1', port=5000, debug=True)
+    # app.run(host='0.0.0.0', port=80, debug=True)
+    app.run(host='127.0.0.1', port=5000, debug=True)
 
 
 def signal_handler(sig, frame):
