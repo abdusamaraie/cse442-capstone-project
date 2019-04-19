5b2f0b24fa7672153552c9dc0dac35db4ed75768
Baily Troyer <bailytroyer@Bailys-MacBook-Pro.local>
2019-04-19 16:47:51 -0400

    adding new icon for map view and also adding initial files to work with views with maps

---
+++ Front-end/MapChat/MapChat/Storyboards/Main/Views/Maps/MapView.swift
@@ -0,0 +1,17 @@
+//
+//  MapView.swift
+//  MapChat
+//
+//  Created by Baily Troyer on 4/19/19.
+//  Copyright © 2019 CSE442Group. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import GoogleMaps
+
+class MapView: UIViewController {
+ 
+    override func loadView() {
+    }
+}
