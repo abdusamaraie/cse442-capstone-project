8cb7ce5368aa53300455b7e3e147896174dc839c
Baily Troyer <bailytroyer@Bailys-MacBook-Pro.local>
2019-04-20 13:53:26 -0400

    adding API key along with basic map view delegates to show pin in random location

--- Front-end/MapChat/MapChat/Storyboards/Main/Views/Maps/MapView.swift
+++ Front-end/MapChat/MapChat/Storyboards/Main/Views/Maps/MapView.swift
@@ -12,6 +12,24 @@ import GoogleMaps
 
 class MapView: UIViewController {
  
-    override func loadView() {
+    @IBOutlet weak var mapView: GMSMapView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let camera = GMSCameraPosition.camera(withLatitude: 37.36, longitude: -122.0, zoom: 6.0)
+        mapView.camera = camera
+        showMarker(position: camera.target)
     }
+    
+    func showMarker(position: CLLocationCoordinate2D){
+        let marker = GMSMarker()
+        marker.position = position
+        marker.title = "Palo Alto"
+        marker.snippet = "San Francisco"
+        marker.map = mapView
+    }
+}
+
+extension MapView: GMSMapViewDelegate{
+
 }
