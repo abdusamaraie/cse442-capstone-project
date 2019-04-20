315b1e459d0746162457652aa96af3a55867973c
Darren Matthew <djmatthe@buffalo.edu>
2019-04-20 12:03:37 -0400

    removed tests from cloudbuild

--- back-end/server/constants/constants.py
+++ back-end/server/constants/constants.py
@@ -1,6 +1,6 @@
 UPLOAD_PATH = 'database/uploads/'
 
-NEO4J_CLUSTER_IP = "35.232.24.4"
+NEO4J_CLUSTER_IP = "34.66.225.189"
 
 # temporary api key for testing
 PLACES_API_KEY = 'AIzaSyAYDeO-TqsstxXzmZqCes-J3exBDQgD5cY'
