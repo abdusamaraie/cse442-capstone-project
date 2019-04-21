76d13b9c913fd7401a15669d7cff08c78ee27c3c
Baily Troyer <bailytroyer@Bailys-MacBook-Pro.local>
2019-04-21 12:08:09 -0400

    fixing bug with updating location when accuracy is set too specific, causing the view to always update interrupting user interaction

--- Front-end/MapChat/MapChat/Storyboards/Main/Views/Maps/MapView.swift
+++ Front-end/MapChat/MapChat/Storyboards/Main/Views/Maps/MapView.swift
@@ -10,15 +10,133 @@ import Foundation
 import UIKit
 import GoogleMaps
 
-class MapView: UIViewController {
+let kMapStyle = "[" +
+    "  {" +
+    "    \"featureType\": \"poi.business\"," +
+    "    \"elementType\": \"all\"," +
+    "    \"stylers\": [" +
+    "      {" +
+    "        \"visibility\": \"off\"" +
+    "      }" +
+    "    ]" +
+    "  }," +
+    "  {" +
+    "    \"featureType\": \"transit\"," +
+    "    \"elementType\": \"labels.icon\"," +
+    "    \"stylers\": [" +
+    "      {" +
+    "        \"visibility\": \"off\"" +
+    "      }" +
+    "    ]" +
+    "  }" +
+"]"
+
+class MapView: UIViewController, CLLocationManagerDelegate {
  
     @IBOutlet weak var mapView: GMSMapView!
+    var locationManager = CLLocationManager()
+    
+    //
+    
+    var startLocation: CLLocation!
+    var lastLocation: CLLocation!
+    var startDate: Date!
+    var traveledDistance: Double = 0
+    
+    // Set the status bar style to complement night-mode.
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        return .lightContent
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        let camera = GMSCameraPosition.camera(withLatitude: 37.36, longitude: -122.0, zoom: 6.0)
-        mapView.camera = camera
-        showMarker(position: camera.target)
+        
+//        let camera = GMSCameraPosition.camera(withLatitude: -33.86, longitude: 151.20, zoom: 14.0)
+//        let mapView = GMSMapView.map(withFrame: CGRect.zero, camera: camera)
+//        //let mapView = GMSMapView.map(withFrame: CGRect.zero, camera: camera)
+//
+//        do {
+//            // Set the map style by passing a valid JSON string.
+//            mapView.mapStyle = try GMSMapStyle(jsonString: kMapStyle)
+//        } catch {
+//            NSLog("One or more of the map styles failed to load. \(error)")
+//        }
+//
+//        self.view = mapView
+        
+        // User Location
+        locationManager.delegate = self
+        locationManager.requestWhenInUseAuthorization()
+        //locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        
+        locationManager.desiredAccuracy = kCLLocationAccuracyBestForNavigation
+        locationManager.distanceFilter = 10
+        
+        locationManager.startUpdatingLocation()
+    }
+    
+//    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+//        let userLocation = locations.last
+//        let center = CLLocationCoordinate2D(latitude: userLocation!.coordinate.latitude, longitude: userLocation!.coordinate.longitude)
+//
+//        let camera = GMSCameraPosition.camera(withLatitude: userLocation!.coordinate.latitude,
+//                                              longitude: userLocation!.coordinate.longitude, zoom: 13.0)
+//        mapView = GMSMapView.map(withFrame: CGRect.zero, camera: camera)
+//        mapView.isMyLocationEnabled = true
+//        self.view = mapView
+//
+////        let marker = GMSMarker()
+////        marker.position = CLLocationCoordinate2D(latitude: userLocation!.coordinate.latitude, longitude: userLocation!.coordinate.longitude)
+////        marker.title = "Home"
+////        marker.snippet = "hey there :)"
+////        marker.map = mapView
+//
+//        locationManager.stopUpdatingLocation()
+//    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation])
+    {
+        
+        if startDate == nil {
+            startDate = Date()
+        } else {
+            print("elapsedTime:", String(format: "%.0fs", Date().timeIntervalSince(startDate)))
+        }
+        if startLocation == nil {
+            startLocation = locations.first
+            updateMap(manager, locations)
+        } else if let location = locations.last {
+            traveledDistance += lastLocation.distance(from: location)
+            print("Traveled Distance:",  traveledDistance)
+            print("Straight Distance:", startLocation.distance(from: locations.last!))
+            
+            if (startLocation.distance(from: locations.last!) > 15) {
+                updateMap(manager, locations)
+            }
+        }
+        lastLocation = locations.last
+
+        //manager.stopUpdatingLocation()
+    }
+    
+    func updateMap(_ manager: CLLocationManager, _ locations: [CLLocation]) {
+        print("----refreshing----")
+        print(locations)
+        //let currentLocation =     CLLocationCoordinate2D(latitude:CLLocationDegrees(locations[0].coordinate.latitude), longitude:CLLocationDegrees(locations[0].coordinate.longitude))
+        self.mapView.camera = GMSCameraPosition.camera(withTarget:locations[0].coordinate, zoom: 10.0)
+        do {
+            // Set the map style by passing a valid JSON string.
+            mapView.mapStyle = try GMSMapStyle(jsonString: kMapStyle)
+        } catch {
+            NSLog("One or more of the map styles failed to load. \(error)")
+        }
+        print("zooming")
+        self.mapView.animate(toZoom: 18)
+        self.mapView.isMyLocationEnabled = true
+        
+        showMarker(position: locations[0].coordinate)
+        
+        manager.stopUpdatingLocation()
     }
     
     func showMarker(position: CLLocationCoordinate2D){
