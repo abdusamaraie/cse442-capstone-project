79dc53627ecbc087e05ad69268c6ffda34cb45d8
Baily Troyer <bailytroyer@Bailys-MacBook-Pro.local>
2019-04-22 19:53:44 -0400

    adding setting option that allows users to upvote and downvote messages in feed

--- Front-end/MapChat/MapChat/Storyboards/Main/Views/Feed/GroupView.swift
+++ Front-end/MapChat/MapChat/Storyboards/Main/Views/Feed/GroupView.swift
@@ -25,6 +25,8 @@ class GroupView: UIViewController, UITableViewDataSource, UITableViewDelegate, C
     
     var group_list:[GroupPostManager.GroupObject] = []
     
+    var currentLocation:CLLocation!
+    
 //    var group_list:[GroupPostManager.GroupObject]! {
 //        didSet{
 //            groupTableView.reloadData()
@@ -55,22 +57,16 @@ class GroupView: UIViewController, UITableViewDataSource, UITableViewDelegate, C
         // Code to refresh table view
         loadFeed()
         refreshControl.endRefreshing()
-        // cardView.card.backgroundImage
     }
     
     override func viewDidAppear(_ animated: Bool) {
-//        if let indexPath = groupTableView.indexPathForSelectedRow {
-//            groupTableView.deselectRow(at: indexPath, animated: true)
-//        }
+        if let indexPath = groupTableView.indexPathForSelectedRow {
+            groupTableView.deselectRow(at: indexPath, animated: true)
+        }
         loadFeed()
     }
     
-//    override func viewWillAppear(_ animated: Bool) {
-//        loadFeed()
-//    }
-    
     func wipe_feed() {
-        print("wiping feed")
         self.group_list = []
     }
     
@@ -82,7 +78,6 @@ class GroupView: UIViewController, UITableViewDataSource, UITableViewDelegate, C
         
         wipe_feed()
         
-        print("requesting location")
         locManager.requestWhenInUseAuthorization()
         if((CLLocationManager.authorizationStatus() == .authorizedWhenInUse) || (CLLocationManager.authorizationStatus() ==  .authorizedAlways)) {
             print("loaction requested... now loading loc manager")
@@ -98,34 +93,40 @@ class GroupView: UIViewController, UITableViewDataSource, UITableViewDelegate, C
         // let sv = UIViewController.displaySpinner(onView: self.view)
         
         if let location = locations.first {
-            
-            // self.navigationItem.title = "\(location.coordinate.latitude), \(location.coordinate.longitude)"
-            
-            GroupPostManager.sharedInstance.latitude = "\(location.coordinate.latitude)"
-            GroupPostManager.sharedInstance.longitude = "\(location.coordinate.longitude)"
-            
-            // displau spinner
-            
-            GroupPostManager.sharedInstance.getGroupData(completion: {(response) in
-                
-                let response_object: [GroupPostManager.GroupObject] = response
-                
-                // print("response object_group view: \(response_object)")
-                // print("count_group view: \(response_object.count)")
-                
-                if (response_object.count != 0) {
-                    // print("response: \(response)")
-                    
-                    self.group_list = response
-                    // UIViewController.removeSpinner(spinner: sv)
-                    self.groupTableView.reloadData()
-                } else {
-                    // print("RESPONSE WAS NIL")
-                    // UIViewController.removeSpinner(spinner: sv)
-                }
-            })
-            
+            currentLocation = location
+            updateLocation()
         }
+    }
+    
+    func updateLocation() {
+        print("locations first")
+        
+        // self.navigationItem.title = "\(location.coordinate.latitude), \(location.coordinate.longitude)"
+        
+        GroupPostManager.sharedInstance.latitude = "\(currentLocation.coordinate.latitude)"
+        GroupPostManager.sharedInstance.longitude = "\(currentLocation.coordinate.longitude)"
+        
+        // displau spinner
+        let sv = UIViewController.displaySpinner(onView: self.view)
+        
+        GroupPostManager.sharedInstance.getGroupData(completion: {(response) in
+            
+            let response_object: [GroupPostManager.GroupObject] = response
+            
+            // print("response object_group view: \(response_object)")
+            // print("count_group view: \(response_object.count)")
+            
+            if (response_object.count != 0) {
+                print("response-------->: \(response_object)")
+                
+                self.group_list = response_object
+                UIViewController.removeSpinner(spinner: sv)
+                self.groupTableView.reloadData()
+            } else {
+                print("RESPONSE WAS NIL")
+                UIViewController.removeSpinner(spinner: sv)
+            }
+        })
     }
         
     func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
@@ -134,60 +135,55 @@ class GroupView: UIViewController, UITableViewDataSource, UITableViewDelegate, C
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         print("count: \(group_list.count)")
-        // return group_list.count
-        return 4
+        return group_list.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
-//        let group_item = group_list[indexPath.row]
-//
-//        let cell = tableView.dequeueReusableCell(withIdentifier: "GroupViewCell", for: indexPath as IndexPath) as! GroupViewCell
-//
-//        // cell.groupImage.image = #imageLiteral(resourceName: "davis_hall")
-//
-//        let url = URL(string: group_item.URL!)
-//
-//        DispatchQueue.global().async {
-//            let data = try? Data(contentsOf: url!) //make sure your image in this url does exist, otherwise unwrap in a if let check / try-catch
-//            DispatchQueue.main.async {
-//                cell.groupImage.image = UIImage(data: data!)
-//                cell.groupImage.contentMode = .scaleToFill
-//                // cell.groupImage.alpha = 0.35
-//            }
-//        }
-//
-//        cell.buildingName.text = group_list[indexPath.row].name
-//
-//        return cell
-        
         let cell = tableView.dequeueReusableCell(withIdentifier: "TestCard", for: indexPath as IndexPath) as! CardCell
         
-        cell.card.backgroundImage = #imageLiteral(resourceName: "davis_hall")
-        // cell.card.backgroundImage = cell.card.backgroundImage?.tint(with: #colorLiteral(red: 0.5749713182, green: 0.596922338, blue: 0.5967071056, alpha: 0.1506580654))
+        let group_item = group_list[indexPath.row]
+        let url = URL(string: group_item.URL!)
+
+        DispatchQueue.global().async {
+            let data = try? Data(contentsOf: url!) //make sure your image in this url does exist, otherwise unwrap in a if let check / try-catch
+            DispatchQueue.main.async {
+                cell.card.backgroundImage = UIImage(data: data!)
+                // cell.groupImage.alpha = 0.35
+            }
+        }
+        
+        
+        // darken image of
         cell.card.backgroundImage = cell.card.backgroundImage?.alpha(0.75)
         cell.card.backgroundImage = cell.card.backgroundImage?.darkened()
         
         let detailVC = storyboard?.instantiateViewController(withIdentifier: "CardContent")
+        let detailVC_type = detailVC as! FeedView
+        detailVC_type.place_id = group_item.ID!
+        
+        // set values
+        cell.card.title = group_item.name!
+        cell.card.itemTitle = "University at Buffalo"
+        cell.card.itemSubtitle = "123 posts"
     
         cell.card.shouldPresent(detailVC, from: self, fullscreen: true)
-        //cell.card.detailView = detailVC?.view
         
         return cell
     }
     
-    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        print("index path: \(indexPath.row)")
-        print("group list: \(group_list)")
-        if (group_list.count > 0) {
-            self.selectedGroup = group_list[indexPath.row]
-            GroupPostManager.sharedInstance.current_group = self.selectedGroup
-            self.performSegue(withIdentifier: "toGroup", sender: self)
-            groupTableView.deselectRow(at: indexPath, animated: true)
-        } else {
-            print("not yet")
-        }
-    }
+//    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+//        print("index path: \(indexPath.row)")
+//        print("group list: \(group_list)")
+//        if (group_list.count > 0) {
+//            self.selectedGroup = group_list[indexPath.row]
+//            GroupPostManager.sharedInstance.current_group = self.selectedGroup
+//            self.performSegue(withIdentifier: "toGroup", sender: self)
+//            groupTableView.deselectRow(at: indexPath, animated: true)
+//        } else {
+//            print("not yet")
+//        }
+//    }
     
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
