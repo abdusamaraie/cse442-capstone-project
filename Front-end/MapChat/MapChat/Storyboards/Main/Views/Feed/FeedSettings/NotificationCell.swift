2031d77d7ae1a7b2228f5e3c10fe05609f0b535b
Baily Troyer <bailytroyer@Bailys-MacBook-Pro.local>
2019-04-21 22:20:37 -0400

    adding settings view allowing user to eventaully set the radius along with other specific permissions for new features

---
+++ Front-end/MapChat/MapChat/Storyboards/Main/Views/Feed/FeedSettings/NotificationCell.swift
@@ -0,0 +1,21 @@
+//
+//  NotificationCell.swift
+//  MapChat
+//
+//  Created by Baily Troyer on 4/21/19.
+//  Copyright © 2019 CSE442Group. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class NotificationCell: UITableViewCell {
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+    
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+    }
+}
