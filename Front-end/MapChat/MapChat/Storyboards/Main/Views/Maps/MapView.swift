024ab95a48d72a9680b9ed01adeb8b755dbc5277
Baily Troyer <bailytroyer@Bailys-MacBook-Pro.local>
2019-04-23 12:15:17 -0400

    adding pin refresh calling similar function used in generating feed, this way you can view on a map the locations dropped

--- Front-end/MapChat/MapChat/Storyboards/Main/Views/Maps/MapView.swift
+++ Front-end/MapChat/MapChat/Storyboards/Main/Views/Maps/MapView.swift
@@ -8,6 +8,7 @@
 
 import Foundation
 import UIKit
+import SwiftyJSON
 import GoogleMaps
 
 let kMapStyle = "[" +
@@ -52,19 +53,6 @@ class MapView: UIViewController, CLLocationManagerDelegate {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-//        let camera = GMSCameraPosition.camera(withLatitude: -33.86, longitude: 151.20, zoom: 14.0)
-//        let mapView = GMSMapView.map(withFrame: CGRect.zero, camera: camera)
-//        //let mapView = GMSMapView.map(withFrame: CGRect.zero, camera: camera)
-//
-//        do {
-//            // Set the map style by passing a valid JSON string.
-//            mapView.mapStyle = try GMSMapStyle(jsonString: kMapStyle)
-//        } catch {
-//            NSLog("One or more of the map styles failed to load. \(error)")
-//        }
-//
-//        self.view = mapView
-        
         // User Location
         locationManager.delegate = self
         locationManager.requestWhenInUseAuthorization()
@@ -76,24 +64,12 @@ class MapView: UIViewController, CLLocationManagerDelegate {
         locationManager.startUpdatingLocation()
     }
     
-//    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
-//        let userLocation = locations.last
-//        let center = CLLocationCoordinate2D(latitude: userLocation!.coordinate.latitude, longitude: userLocation!.coordinate.longitude)
-//
-//        let camera = GMSCameraPosition.camera(withLatitude: userLocation!.coordinate.latitude,
-//                                              longitude: userLocation!.coordinate.longitude, zoom: 13.0)
-//        mapView = GMSMapView.map(withFrame: CGRect.zero, camera: camera)
-//        mapView.isMyLocationEnabled = true
-//        self.view = mapView
-//
-////        let marker = GMSMarker()
-////        marker.position = CLLocationCoordinate2D(latitude: userLocation!.coordinate.latitude, longitude: userLocation!.coordinate.longitude)
-////        marker.title = "Home"
-////        marker.snippet = "hey there :)"
-////        marker.map = mapView
-//
-//        locationManager.stopUpdatingLocation()
-//    }
+    override func viewDidAppear(_ animated: Bool) {
+        if lastLocation != nil {
+            print("UPDATING PINS")
+            getGroups(lastLocation)
+        }
+    }
     
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation])
     {
@@ -121,7 +97,10 @@ class MapView: UIViewController, CLLocationManagerDelegate {
     }
     
     func updateMap(_ manager: CLLocationManager, _ locations: [CLLocation]) {
+        
         print("----refreshing----")
+        getGroups(locations[0])
+        
         print(locations)
         //let currentLocation =     CLLocationCoordinate2D(latitude:CLLocationDegrees(locations[0].coordinate.latitude), longitude:CLLocationDegrees(locations[0].coordinate.longitude))
         self.mapView.camera = GMSCameraPosition.camera(withTarget:locations[0].coordinate, zoom: 10.0)
@@ -135,7 +114,7 @@ class MapView: UIViewController, CLLocationManagerDelegate {
         self.mapView.animate(toZoom: 18)
         self.mapView.isMyLocationEnabled = true
         
-        showMarker(position: locations[0].coordinate)
+        // showMarker(position: locations[0].coordinate, message: <#String#>)
         
         // update radius custom
         let update = GMSCameraUpdate.fit(GMSCircle(position: locations[0].coordinate, radius: CLLocationDistance(50)).bounds())
@@ -145,11 +124,69 @@ class MapView: UIViewController, CLLocationManagerDelegate {
         manager.stopUpdatingLocation()
     }
     
-    func showMarker(position: CLLocationCoordinate2D){
+    func getGroups(_ location: CLLocation) {
+        
+        // clear mapview to load newly dropped things
+        mapView.clear()
+        
+        GroupPostManager.sharedInstance.latitude = "\(location.coordinate.latitude)"
+        GroupPostManager.sharedInstance.longitude = "\(location.coordinate.longitude)"
+        
+        GroupPostManager.sharedInstance.getGroupData(completion: {(response) in
+            
+            let response_object: [GroupPostManager.GroupObject] = response
+            
+            if (response_object.count != 0) {
+                // print("response-------->: \(response_object)")
+                for value in response_object {
+        
+                    if (value.ID != nil) {
+                        print("GROUP ID: \(value.ID!)")
+                        //---
+                        GroupPostManager.sharedInstance.current_group.ID = value.ID!
+                        
+                        GroupPostManager.sharedInstance.assignLatLong(latitde: "\(location.coordinate.latitude)", longitude: "\(location.coordinate.longitude)")
+                        
+                        GroupPostManager.sharedInstance.getPostData(completion: {(response) in
+                            
+                            let posts: JSON = response
+                            
+                            print("posts: \(posts)")
+                            
+                            for (_, post) in posts {
+                                print("POST: -> \(post)")
+                                let message = post["content"].string
+                                let username = post["username"].string
+                                
+                                let lat = post["latitude"].double
+                                let lon = post["longitude"].double
+                                
+                                let coordinates = CLLocationCoordinate2D(latitude:lat!, longitude:lon!)
+                                
+                                self.showMarker(position: coordinates, message: message!, username: username!)
+                                // self.messages.append(Message(message: post["content"].string!, tag: "#UBISSHIT", numberLikes: 20))
+                            }
+                            
+                            // self.feedView.reloadData()
+                        })
+                        //---
+                    }
+                    
+                }
+
+            } else {
+                print("RESPONSE WAS NIL")
+                // UIViewController.removeSpinner(spinner: sv)
+            }
+        })
+    }
+    
+    func showMarker(position: CLLocationCoordinate2D, message:String, username:String){
+        print("dropping marker")
         let marker = GMSMarker()
         marker.position = position
-        marker.title = "Palo Alto"
-        marker.snippet = "San Francisco"
+        marker.title = message
+        marker.snippet = username
         marker.map = mapView
     }
 }
