de0a8e33820b02cbaf7349859eb5fbb04c6461b8
Darren Matthew <djmatthe@buffalo.edu>
2019-04-24 13:36:28 -0400

    changed default place photo

--- back-end/server/constants/constants.py
+++ back-end/server/constants/constants.py
@@ -7,7 +7,7 @@ EARTH_RADIUS_METERS = 6371000
 
 # temporary api key for testing
 PLACES_API_KEY = 'AIzaSyAYDeO-TqsstxXzmZqCes-J3exBDQgD5cY'
-DEFAULT_PLACE_PHOTO = 'https://i.kym-cdn.com/entries/icons/original/000/025/999/Screen_Shot_2018-04-24_at_1.33.44_PM.png'
+DEFAULT_PLACE_PHOTO = 'https://www.htmlcsscolor.com/preview/gallery/005BBB.png'
 
 # for places tagged with the place 'Other'
 OTHER_PHOTO_URL = ''
