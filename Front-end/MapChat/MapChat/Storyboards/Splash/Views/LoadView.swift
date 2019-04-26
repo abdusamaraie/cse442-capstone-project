483acf8c119805d7c5fac4fd3fe3d9298f221e7a
Darren Matthew <djmatthe@buffalo.edu>
2019-04-26 10:29:46 -0400

    made animation 60fps

--- Front-end/MapChat/MapChat/Storyboards/Splash/Views/LoadView.swift
+++ Front-end/MapChat/MapChat/Storyboards/Splash/Views/LoadView.swift
@@ -15,13 +15,15 @@ class LoadView: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        if let animationView:AnimationView = AnimationView(name: "cachr_white_half") {
+        if let animationView:AnimationView = AnimationView(name: "cachr_white_half_60") {
             animationView.frame = CGRect(x: 0, y: 0, width: 1000, height: 110)
             
             // label.center.y = view.center.y
             animationView.center.x = self.view.center.x
             animationView.center.y = self.view.center.y
             animationView.contentMode = .scaleAspectFill
+            
+            animationView.respectAnimationFrameRate = true
             
             self.view.addSubview(animationView)
             
