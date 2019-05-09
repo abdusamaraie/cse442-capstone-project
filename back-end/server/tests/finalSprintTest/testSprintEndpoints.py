0527882a31af889a6772fb715f0ed19f89e2e7c3
hard2code <aalsamma@buffalo.edu>
2019-05-08 21:27:24 -0400

    add unitest for getting user history of rating, replies and checking if user did rate

--- back-end/server/tests/finalSprintTest/testSprintEndpoints.py
+++ back-end/server/tests/finalSprintTest/testSprintEndpoints.py
@@ -25,22 +25,16 @@ class TestSprintEndpoints(unittest.TestCase):
         self.location = {'latitude': 43.0100431, 'longitude': -78.8012356}
         self.exp_time = str((datetime.now().astimezone(es) + timedelta(days=7)))
         self.password_hash = authenticate.generate_hash(self.uname, self.pwd)
-
-    def test_1_user_signup(self):
-
-        user = neo4j.get_user(self.uname)
-        if user == self.uname:
-            #delete user if already exists
-            print("Deleting user: \n")
-            neo4j.delete_user(self.uname, self.password_hash)
-
-        user = User(self.uname, self.fn, self.ln,self.email,
-                    self.password_hash,self.brd,self.hometown)
-        # test adding user
-        self.assertTrue(neo4j.add_user(user))
+        user = User(self.uname, self.fn, self.ln, self.email,
+                    self.password_hash, self.brd, self.hometown)
+        neo4j.delete_user(self.uname, self.password_hash)
+        neo4j.add_user(user)
+        self.post_id = neo4j.post_message(self.uname, self.location,
+                           self.msg, self.exp_time, self.place_id)
 
 
-    def test_2_user_profile(self):
+
+    def test_1_get_user_profile(self):
         # Post request payload to server endpoint
         payload = {'username': 'admin'}
         #test ip. I can also test for neo4j cluster ip
@@ -51,16 +45,18 @@ class TestSprintEndpoints(unittest.TestCase):
 
         self.assertTrue(r.text)
 
-    def test_3_posting_messages(self):
+    def test_2_get_user_setting(self):
+        payload = {'username': self.uname}
+        url = 'http://127.0.0.1:5000/settings'
 
-        print("Posting A message: \n")
+        r = requests.get(url, params=payload)
+        self.assertEqual(r.status_code, 200)
+        print("Getting user Profile setting: \n" + str(r.text) + "\n")
+        self.assertTrue(r.text)
 
-        post_id = neo4j.post_message(self.uname, self.location,
-                                     self.msg, self.exp_time, self.place_id)
-        print("post id: " + str(post_id) + "\n")
-        self.assertNotEqual(post_id, None)
 
-    def test_4_get_place(self):
+
+    def test_3_get_place(self):
         payload = {'lat': self.location['latitude'],
                    'long': self.location['longitude'],
                    'radius':self.radius}
@@ -71,30 +67,82 @@ class TestSprintEndpoints(unittest.TestCase):
         print("Getting a place : \n" + str(r.text) + "\n")
         self.assertTrue(r.text)
 
-    def test_5_get_message_at_place(self):
+    def test_4_get_message_at_place(self):
         payload = {'placeId':self.place_id}
-        url = 'http://127.0.0.1:5000//place/message'
+        url = 'http://127.0.0.1:5000/place/message'
         r = requests.get(url=url, params=payload)
         self.assertEqual(r.status_code, 200)
         print("Getting a message at a place: \n" +str(r.text)+ "\n")
 
         self.assertTrue(r.text)
 
+    def test_5_get_user_post_history(self):
 
-    # def test_y_delete_user(self):
-    #
-    #     user = neo4j.get_user(self.uname)
-    #     self.assertEqual(user,self.uname)
-    #     # delete user if already exists
-    #     neo4j.delete_user(self.uname, self.password_hash)
+        payload = {'username': self.uname}
+        url = 'http://127.0.0.1:5000/history/posts'
 
-#    Verify that database has been whipped
+        r = requests.get(url, params=payload)
+        self.assertEqual(r.status_code, 200)
+        print("Getting user posts history: \n" + str(r.text) + "\n")
+        self.assertTrue(r.text)
+
+    def test_6_get_user_ratings(self):
+        payload = {'username': self.uname}
+        url = 'http://127.0.0.1:5000/history/ratings'
+
+
+        relation = "LIKED"
+        self.msg = "This message for a reply"
+
+        neo4j.rate_post(self.post_id,relation,self.uname)
+
+        r = requests.get(url, params=payload)
+        self.assertEqual(r.status_code, 200)
+        print("Getting user rating: \n" + str(r.text) + "\n")
+        self.assertTrue(r.text)
+
+    def test_7_check_If_user_didrare(self):
+
+        payload = {'postId': self.post_id,
+                   'username': self.uname}
+
+        relation = "DISLIKED"
+        self.msg = "This message for a rating"
+
+        neo4j.rate_post(self.post_id, relation, self.uname)
+
+        url = 'http://127.0.0.1:5000/didrate'
+        r = requests.get(url, params=payload)
+
+        self.assertEqual(r.status_code, 200)
+        print("Checking if user did rate a post: \n" + str(r.text) + "\n")
+        self.assertTrue(r.text)
+
+
+    def test_8_get_user_reply_history(self):
+        payload = {'username': self.uname}
+        url = 'http://127.0.0.1:5000/history/replies'
+
+        self.msg = "This is a second reply"
+
+        self.assertTrue(neo4j.reply_to_post(self.msg, self.post_id, self.uname))
+
+        r = requests.get(url, params=payload)
+        self.assertEqual(r.status_code, 200)
+        print("Getting user reply history: \n" + str(r.text) + "\n")
+        self.assertTrue(r.text)
+
+
+
+
+   #Verify that database has been whipped
     def test_z_wipe_database(self):
         # test ip. I can also test for neo4j cluster ip
         url = 'http://127.0.0.1:5000/neo4j'
         r = requests.delete(url=url)
-
-        print("Whipping database: " + str(r.text) +"\n")
+        print("Deleting user: \n")
+        neo4j.delete_user(self.uname, self.password_hash)
+        print("Cleaning database: " + str(r.text) +"\n")
         self.assertTrue(r.text)
 
 if __name__ == '__main__':
